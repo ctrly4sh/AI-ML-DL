2a701908d68631e64d2a8d08fb14b875dd42e860
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-02-17 22:52:31 +0530

    Basic-Statistics.py
    added standard deviation

--- Basic-Statistics.py
+++ Basic-Statistics.py
@@ -8,29 +8,39 @@ DATA_set = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 resultMean = Mean(DATA_set)
 # print(resultMean)
 
- def Median(DATA):
+def Median(DATA):
     import numpy as np
     meanResult = np.median(DATA)
     return meanResult
 
 DATA_set = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 resultMedeain = Median(DATA_set) 
-# print(resultMedeain)
-
-DATA_set = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-resultMedeain = Median(DATA_set) 
-# print(resultMedeain)
+print(resultMedeain)
 
 
 # Median using numpy in python
 def Mode(DATA):
-    from scipy import stats # install scipy using pip , numpy doesnt have a function for mode  
+    from scipy import stats 
     modeResult = stats.mode(DATA)
     return modeResult
 
 DATA_set = [99,99,99,99,99,86,87,88,111,86,103,87,94,78,77,85,86]
 resultMode = Mode(DATA_set)
 # print(resultMode)
+
+#Standard Deviation
+
+def StandardDeviation(DATA):
+    import numpy as np
+    devaition = np.std(DATA)
+    return devaition
+
+speed = [86,87,88,86,87,85,86]
+resultDeviation = StandardDeviation(speed)
+print(resultDeviation)
+
+
+
 
 
 
