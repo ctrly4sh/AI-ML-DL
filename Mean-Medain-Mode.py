193c4666117560badef3fa26459fd74dc82f4622
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-02-17 22:37:03 +0530

    Add files via upload

---
+++ Mean-Medain-Mode.py
@@ -0,0 +1,29 @@
+#Mean using numpy in python
+def Mean(DATA):
+    import numpy as np
+    meanResult = np.mean(DATA)
+    return meanResult
+
+DATA_set = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+resultMean = Mean(DATA_set)
+# print(resultMean)
+
+ 
+
+DATA_set = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+resultMedeain = Median(DATA_set) 
+# print(resultMedeain)
+
+
+# Median using numpy in python
+def Mode(DATA):
+    from scipy import stats 
+    modeResult = stats.mode(DATA)
+    return modeResult
+
+DATA_set = [99,99,99,99,99,86,87,88,111,86,103,87,94,78,77,85,86]
+resultMode = Mode(DATA_set)
+# print(resultMode)
+
+
+
