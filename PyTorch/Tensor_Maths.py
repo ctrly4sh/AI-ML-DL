c954dd401a7ebcd2147170a870cd24a813174304
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-04-11 23:27:47 +0530

    Tensor Mathematics

--- PyTorch/Tensor_Maths.py
+++ PyTorch/Tensor_Maths.py
@@ -1,10 +1,9 @@
 # Tensor maths and Comparison
-
 import torch
 import numpy as np
-
 x = torch.tensor([1,2,3])
 y = torch.tensor([4,5,6])
+
 
 #addition
 ADDITON = torch.add(x,y)
@@ -14,14 +13,13 @@ print("The Sum of the Tensors is : "  , ADDITON)
 SUBSTRACTION = torch.subtract(x,y) 
 print("The Substractoin of the Tensors are : " , SUBSTRACTION)
 
-#Division
+#Division 
 DIVISION = torch.true_divide(x,y)
 print("Division is " , DIVISION)
 
 #inplace operation
 t = torch.zeros(3)
 t.add_(x) 
-
 
 #Exponentiation
 z = x.pow(2)
@@ -35,6 +33,21 @@ z = x < 0
 #Matrix Multiplication
 x1 = torch.rand((2,5))
 x2 = torch.rand((5,3))
-
 tensor_mult = torch.mm(x1,x2)
 
+#Matrix exponentiation
+matrix_exp = torch.rand(5,5)
+print(matrix_exp.matrix_power(3))
+
+#Element wise multplication
+
+'''
+x = 1,2,3
+y = 4,5,6
+1 x 4 = 2
+2 x 5 = 10
+3 x 6 = 18
+'''
+
+z = x * y
+print(z)
