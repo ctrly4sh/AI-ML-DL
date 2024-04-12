41c3d9fb8ca5f9e8219fd040013e14a73ac5766d
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-04-12 21:42:00 +0530

    LLM from Scratch

---
+++ LLM_from_Scratch/Llm_First.py
@@ -0,0 +1,16 @@
+'''f = open("LLM_from_Scratch\wizard_of_oz.txt" , "r" , encoding="utf-8")
+text = f.read()
+print(text[:200]) # first 200 characters of the text
+
+#extracting all the characters from the text
+characters = sorted(set(text))
+print(characters)
+
+#Tokenizer -> consists of a Encoder and decoder
+string_to_int = {ch:i for i,ch in enumerate(characters)}
+print(string_to_int)'''
+
+temp = {'a','e','i','o','u'}
+
+temp_string = {ch:i for ch,i in enumerate(temp)}
+print(temp_string)
--- LLM_from_Scratch/first.py
+++
@@ -1,9 +0,0 @@
-f = open("wizard_of_oz.txt" , "r" , encoding="utf-8")
-text = f.read()
-print(text[:200]) # first 200 characters of the text
-
-#extracting all the characters from the text
-characters = sorted(set(text))
-print(characters)
-
-#Tokenizer -> Encoder and decorder
