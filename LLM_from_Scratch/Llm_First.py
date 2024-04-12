d6342f79cc04182ab483f6dabafb2e353b592563
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-04-12 21:42:32 +0530

    LLM from Scratch

--- LLM_from_Scratch/Llm_First.py
+++ LLM_from_Scratch/Llm_First.py
@@ -1,4 +1,6 @@
-'''f = open("LLM_from_Scratch\wizard_of_oz.txt" , "r" , encoding="utf-8")
+'''LLM from Scratch'''
+
+f = open("LLM_from_Scratch\wizard_of_oz.txt" , "r" , encoding="utf-8")
 text = f.read()
 print(text[:200]) # first 200 characters of the text
 
@@ -8,7 +10,7 @@ print(characters)
 
 #Tokenizer -> consists of a Encoder and decoder
 string_to_int = {ch:i for i,ch in enumerate(characters)}
-print(string_to_int)'''
+print(string_to_int)
 
 temp = {'a','e','i','o','u'}
 
