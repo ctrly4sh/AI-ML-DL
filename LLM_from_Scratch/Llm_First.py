484b69d2589f562058901f75e008f25ebdc904fe
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-04-20 18:32:42 +0530

    encoders and decoders

--- LLM_from_Scratch/Llm_First.py
+++ LLM_from_Scratch/Llm_First.py
@@ -1,3 +1,5 @@
+import torch 
+
 '''LLM from Scratch'''
 
 f = open("LLM_from_Scratch\wizard_of_oz.txt" , "r" , encoding="utf-8")
@@ -10,10 +12,18 @@ print(characters)
 
 #Tokenizer -> consists of a Encoder and decoder
 string_to_int = {ch:i for i,ch in enumerate(characters)}
-print(string_to_int)
-
 int_to_string = {i:ch for i,ch in enumerate(characters)}
 
 #lambda function for encoding and decoding 
+encode = lambda s:[string_to_int[c] for c in s]
+encoded = encode("hello")
+
+decode = lambda l: "".join([int_to_string[i] for i in l])
+decoded = decode(encode("hello"))
+
+# print("encoded is " ,encoded , "Decoded is " , decoded )
+
+data = torch.tensor(encode(text) , dtype=torch.long)
+print(data[:100])
 
 
